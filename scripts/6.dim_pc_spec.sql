6fe6c90b677aed0bd374d4e8c3436241e6a71424
Andile Dube <andile.data99@gmail.com>
2026-04-17 15:19:51 +0200

    formatting and cleaning data

--- scripts/6.dim_pc_spec.sql
+++ scripts/6.dim_pc_spec.sql
@@ -1,30 +1,32 @@
-  ---Dim pc spec inserting primary key
-     DROP TABLE [PC_Sales_Staging_dtw].[dbo].[dim_pc_spec]
-     CREATE TABLE [PC_Sales_Staging_dtw].[dbo].[dim_pc_spec](
-     [PC_make_ID]int identity (1,1) primary key,
-	[PC_Make] [nvarchar](50) NOT NULL,
-	[PC_Model] [nvarchar](50) NOT NULL,
-	[Storage_Type] [nvarchar](50) NOT NULL,
-	[RAM] [nvarchar](50) NOT NULL,
-	[Storage_Capacity] [nvarchar](50) NOT NULL,
-     [Load_date] DATETIME DEFAULT GETDATE()
-    )
-
-      ---Inserting data into table 
-
-  INSERT INTO [PC_Sales_Staging_dtw].[dbo].[dim_pc_spec](
-              [PC_Make]
-             ,[PC_Model]
-             ,[Storage_Type]
-             ,[RAM]
-             ,[Storage_Capacity])
-       SELECT DISTINCT
-              [PC_Make]
-             ,[PC_Model]
-             ,[Storage_Type]
-             ,[RAM]
-             ,[Storage_Capacity]
-  FROM  [PC_Sales_Staging_dtw].[dbo].[Raw_PC_Data]
-
-      ---checking if step was implemented
-    SELECT * FROM [PC_Sales_Staging_dtw].[dbo].[dim_pc_spec]
+---Dim pc spec inserting primary key
+DROP TABLE [PC_Sales_Staging_dtw].[dbo].[dim_pc_spec] CREATE TABLE [PC_Sales_Staging_dtw].[dbo].[dim_pc_spec](
+  [PC_make_ID] int identity (1, 1) primary key,
+  [PC_Make] [nvarchar](50) NOT NULL,
+  [PC_Model] [nvarchar](50) NOT NULL,
+  [Storage_Type] [nvarchar](50) NOT NULL,
+  [RAM] [nvarchar](50) NOT NULL,
+  [Storage_Capacity] [nvarchar](50) NOT NULL,
+  [Load_date] DATETIME DEFAULT GETDATE()
+) 
+---Inserting data into table 
+INSERT INTO
+  [PC_Sales_Staging_dtw].[dbo].[dim_pc_spec](
+    [PC_Make],
+    [PC_Model],
+    [Storage_Type],
+    [RAM],
+    [Storage_Capacity]
+  )
+SELECT
+  DISTINCT [PC_Make],
+  [PC_Model],
+  [Storage_Type],
+  [RAM],
+  [Storage_Capacity]
+FROM
+  [PC_Sales_Staging_dtw].[dbo].[Raw_PC_Data] 
+---checking if step was implemented
+SELECT
+  *
+FROM
+  [PC_Sales_Staging_dtw].[dbo].[dim_pc_spec] 
--- scripts/7.dim_priority.sql
+++ scripts/7.dim_priority.sql
@@ -1,18 +1,18 @@
- ---Dim priority inserting primary key
-     DROP TABLE [PC_Sales_Staging_dtw].[dbo].[dim_priority]
-     CREATE TABLE [PC_Sales_Staging_dtw].[dbo].[dim_priority](
-     [Priority_ID]int identity (1,1) primary key,
-	[Priority] [nvarchar](50) NOT NULL,
-    [Load_date] DATETIME DEFAULT GETDATE()
-    )
- 
-      ---Inserting data into table 
-    
-    INSERT INTO [PC_Sales_Staging_dtw].[dbo].[dim_priority](
-             [Priority])
-       SELECT DISTINCT
-             [Priority] 
-       FROM  [PC_Sales_Staging_dtw].[dbo].[Raw_PC_Data]
-
-    ---checking if step was implemented
-    SELECT * FROM [PC_Sales_Staging_dtw].[dbo].[dim_priority]
+---Dim priority inserting primary key
+DROP TABLE [PC_Sales_Staging_dtw].[dbo].[dim_priority] CREATE TABLE [PC_Sales_Staging_dtw].[dbo].[dim_priority](
+      [Priority_ID] int identity (1, 1) primary key,
+      [Priority] [nvarchar](50) NOT NULL,
+      [Load_date] DATETIME DEFAULT GETDATE()
+) 
+---Inserting data into table 
+INSERT INTO
+      [PC_Sales_Staging_dtw].[dbo].[dim_priority]([Priority])
+SELECT
+      DISTINCT [Priority]
+FROM
+      [PC_Sales_Staging_dtw].[dbo].[Raw_PC_Data] 
+---checking if step was implemented
+SELECT
+      *
+FROM
+      [PC_Sales_Staging_dtw].[dbo].[dim_priority]
