c9bfce5ac79f88147ce6d905c976eea922b4ea41
Andile Dube <andile.data99@gmail.com>
2026-05-08 14:51:03 +0200

    updating script

--- scripts/stored_proceedures/sp_create_dim_channel.sql
+++ scripts/stored_proceedures/sp_create_dim_channel.sql
@@ -1,27 +1,25 @@
-CREATE PROCEDURE sp_create_dim_store
+
+
+CREATE PROCEDURE sp_create_dim_channel
 AS
 BEGIN
   -- SQL_statements to be executed
- ---Dim store inserting primary key
-DROP TABLE [PC_Sales_Staging_dtw].[dbo].[dim_store] CREATE TABLE [PC_Sales_Staging_dtw].[dbo].[dim_store](
-     [Shop_ID] int identity (1, 1) primary key,
-     [Shop_Name] [nvarchar](50) NOT NULL,
-     [Shop_Age] [nvarchar](50) NOT NULL,
-     [Load_date] DATETIME DEFAULT GETDATE()
-) ---Inserting data into table 
+ ---Dim channel inserting primary key 
+DROP TABLE [PC_Sales_Staging_dtw].[dbo].[dim_channel] CREATE TABLE [PC_Sales_Staging_dtw].[dbo].[dim_channel](
+  [Channel_ID] int identity (1, 1) primary key,
+  [Channel] [nvarchar](50) NOT NULL,
+  [Load_date] DATETIME DEFAULT GETDATE()
+)
+ ---Inserting data into table 
 INSERT INTO
-     [PC_Sales_Staging_dtw].[dbo].[dim_store](
-          [Shop_Name],
-          [Shop_Age]
-     )
+  [PC_Sales_Staging_dtw].[dbo].[dim_channel]([Channel])
 SELECT
-     DISTINCT [Shop_Name],
-     [Shop_Age]
+  DISTINCT [Channel]
 FROM
-     [PC_Sales_Staging_dtw].[dbo].[Raw_PC_Data] 
+  [PC_Sales_Staging_dtw].[dbo].[Raw_PC_Data] 
 ---checking if step was implemented
 SELECT
-     *
+  *
 FROM
-     [PC_Sales_Staging_dtw].[dbo].[dim_store]
+  [PC_Sales_Staging_dtw].[dbo].[dim_channel]
 END;
